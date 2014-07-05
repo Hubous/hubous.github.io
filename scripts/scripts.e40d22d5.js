41f04730c43bf6e65a9a752e74af68769941f313
ortizvinicius <vinicius.oaw@gmail.com>
2014-07-04 22:08:36 -0300

    first page

---
+++ scripts/scripts.e40d22d5.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("hubousWebApp",["ngAnimate","ngResource","ngRoute","ngTouch"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("hubousWebApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
